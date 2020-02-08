1e949b0c687f00d17058af266be540399cffb09d
fizaan <fizaan@gmail.com>
2020-02-08 00:13:26 -0500

    removed lc3home

--- tiny-vm-C-version/ant/1.init.bat
+++ tiny-vm-C-version/ant/1.init.bat
@@ -1,5 +1,5 @@
 @echo off
 set JAVA_HOME=C:\Users\Alifa\Desktop\mystuff\faiz.dev.root\java8
 set ANT_HOME=C:\Users\Alifa\Desktop\mystuff\faiz.dev.root\src\apache-ant-1.9.7
-set path=%JAVA_HOME%\bin;%ANT_HOME%\bin;%lc3home%;
+set path=%JAVA_HOME%\bin;%ANT_HOME%\bin;
 
