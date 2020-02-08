717b7bd13f8bcb89e23f5bafe1bd9590f402d43b
Faiz <fizaan@gmail.com>
2020-02-08 00:07:39 -0500

    added run.bat

--- run.bat
+++ run.bat
@@ -3,6 +3,6 @@
 set JAVA_HOME=C:\<YOUR_JAVA_HOME_PATH>
 set path=%JAVA_HOME%\bin;
 
-java -cp %cd%\bin; tiny.vm.CPU
+java -cp %cd%\tiny-vm-C-version\bin; tiny.vm.CPU
 
 set /p=Press any key to exit.
